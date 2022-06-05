06605e0f79a2043d92a5ed280abbe635855f0042
chenjiaxin <924461845@qq.com>
2022-06-05 23:43:18 +0800

    默认的

--- mmblock.py
+++ mmblock.py
@@ -32,10 +32,33 @@ class BlackChain:
             # 工作量证明
             'proof': proof,
             # 上一区块的 hash
-            'previous_hash': self.chain[-1]["index"]
+            'previous_hash': self.last_block["index"]
         }
         # 从新置空
         self.current_transactions = []
         # 将区块添加到区块链中，此时交易账本是空，工作量证明是空
         self.chain.append(block)
         return block
+
+    @property
+    def last_block(self):
+        """
+        :return: 区块链中最后一个区块
+        """
+        return self.chain[-1]
+
+    def proof_of_work(self, last_block):
+        '''
+        简单的工作量证明算法
+        找到一个数，使得区块的hash前4位为0
+        :param last_block 上一个区块
+        :return: 特殊的数
+        '''
+
+        # 工作量证明--->穷举法计算出特殊的数
+        last_hash = self.hash(last_block)
+        proof = 0
+        while self.valid_proof(proof, last_hash) is False:
+            proof += 1
+
+        return proof
